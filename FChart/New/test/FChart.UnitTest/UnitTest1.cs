3286d915d04a02d36ea542ef5b01ed74b348c95e
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-15 16:21:22 +0330

    Insert New Update 2021

---
+++ FChart/New/test/FChart.UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace FChart.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
